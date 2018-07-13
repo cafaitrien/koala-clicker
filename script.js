18cb35cfd91db969751825713a28293f16161d60
Michael Cobb <cobbmj@uw.edu>
2018-07-12 19:01:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1. get the element we want (mqybe document.getElementbyID?)
+//query selector only takes first item with class name. Since we only have one class clickable, all is good
+let clickable = document.querySelector("img.clickable");
+clickable.addEventListener("click", clickCounter)
+// 2. elVariableName.click syntax or addEventListener syntax (see debugging project for example)
+// declare variable counter
+let counter = 0;
+console.log("counter: ", counter);
+// click handler function
+function clickCounter(){
+  // track total clicks - increment the counter variable
+  counter++;
+  // output click count to container in DOM
+  console.log("counter", counter)
+
+  // get the container, update its contents
+  let container = document.querySelector("p#clickcount")
+  container.innerHTML = counter;
+}
